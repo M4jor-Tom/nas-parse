50eaf62d62a187b5707949e90edd3c3c7f938c6b
Tom Vautray <tom-vautray@hotmail.fr>
2012-01-01 05:42:56 +0100

    validate-categories-inheritance.sh validates that the inheritence for tags is respected amongst rules for tags

--- validate-categories-inheritance.sh
+++ validate-categories-inheritance.sh
@@ -7,6 +7,25 @@ CATEGORIES_INHERITANCE_DIR=categories-inheritance
 TAGS_CATEGORIES_DIR=tags-categories
 TAGS_INHERITANCE_DIR=tags-inheritance
 
+# Neat thing here:
+# a FATHER tag must be of the CHILD category, and
+# a CHILD tag must be of the MOTHER category
+
+# This is because files in $CATEGORIES_INHERITANCE_DIR and
+# files in $TAGS_INHERITANCE_DIR are defined with inverted logic.
+
+# In $CATEGORIES_INHERITANCE_DIR: category.txt must have line and line categories
+# In $TAGS_INHERITANCE_DIR: fatherTag.txt defines line and line childrenTags
+
+# Example:
+# -> motherCategory: person (file in $TAGS_CATEGORIES_DIR/ for sure)
+# -> daughterCategory: age (file in $TAGS_CATEGORIES_DIR/ for sure)
+# -> fatherTag: adult (inside file $TAGS_CATEGORIES_DIR/*.txt for sure)
+# -> sonTag: someone_cool (inside file $TAGS_CATEGORIES_DIR for sure)
+# There is an error to detect here if $TAGS_INHERITANCE_DIR/adult.txt does not contain "someone_cool"
+# There is an error to detect here if $TAGS_INHERITANCE_DIR/$fatherTag.txt does not contain "$sonTag"
+# -> person_with_no_age has no category of type age
+
 echo
 echo " --- [VALIDATION FOR CATEGORIES INHERITANCE] --- "
 
@@ -15,42 +34,45 @@ for motherCategoryPath in $CATEGORIES_INHERITANCE_DIR/*; do
 
     motherCategory=$(basename $motherCategoryPath | rev | cut -d "." -f 2 | rev)
 
-    # For childCategory of the motherCategory (inside motherCategoryPath)
-    while IFS= read -r childCategory; do
+    # For daughterCategory of the motherCategory (inside motherCategoryPath)
+    while IFS= read -r daughterCategory; do
 
-        echo "[RULE] Tags of category $motherCategory must inherit from a tag of category $childCategory"
+        echo [RULE] Tags of category [$motherCategory] must inherit from a tag of category [$daughterCategory]
 
-        # For each father tag
-        for fatherTagPath in $TAGS_INHERITANCE_DIR/*; do
+        while IFS= read -r sonTag; do
 
-            fatherTag=$(basename $fatherTagPath | rev | cut -d "." -f 2 | rev)
+            # echo {$sonTag} must have a [$daughterCategory] because it\'s a [$motherCategory]
 
-            # Neat thing here:
-            # a FATHER tag must be of the CHILD category, and
-            # a CHILD tag must be of the MOTHER category
+            sonTagsHasDaughterCategoryPath=false
 
-            # This is because files in $CATEGORIES_INHERITANCE_DIR and
-            # files in $TAGS_INHERITANCE_DIR are defined with inverted logic.
+            # $sonTag\'s category is $motherCategory.
+            # The point of this script is to check that
+            # it has a tag of category $daughterCategory.
 
-            # In $CATEGORIES_INHERITANCE_DIR: category.txt must have line and line categories
-            # In $TAGS_INHERITANCE_DIR: fatherTag.txt defines line and line childrenTags
+            # For each father tag
+            for fatherTagPath in $TAGS_INHERITANCE_DIR/*; do
 
-            # A tag must own 1 and 1 category only
-            fatherTagCategoryPath=$(grep -Elr "^$fatherTag$" $TAGS_CATEGORIES_DIR)
+                fatherTag=$(basename $fatherTagPath | rev | cut -d "." -f 2 | rev)
 
-            fatherTagCategory=$(basename $fatherTagCategoryPath | rev | cut -d "." -f 2 | rev)
+                fatherTagCategoryPath=$(grep -Elr "^$fatherTag$" $TAGS_CATEGORIES_DIR)
+                fatherTagCategory=$(basename $fatherTagCategoryPath | rev | cut -d "." -f 2 | rev)
 
-            # $fatherTag is a $fatherTagCategory
+                if [[ $fatherTagCategory == $daughterCategory ]]; then
 
-            while IFS= read -r childTag; do
+                    if grep -qlE "^$sonTag\$" "$TAGS_INHERITANCE_DIR/$fatherTag.txt"; then
 
-                childTagCategoryPath=$(grep -Elr "^$childTag$" $TAGS_CATEGORIES_DIR)
+                        sonTagsHasDaughterCategoryPath=true
+                    fi
+                fi
+            done
 
-                childTagCategory=$(basename $childTagCategoryPath | rev | cut -d "." -f 2 | rev)
+            if [[ $sonTagsHasDaughterCategoryPath = false ]]; then
 
-                echo $childTag is a $childTagCategory of $fatherTagCategory $fatherTag
+                echo {$sonTag} does not have a [$daughterCategory] but it\'s a [$motherCategory]
+            fi
 
-            done < $fatherTagPath
-        done
+            # echo $sonTag is a $motherCategory of $fatherTagCategory $fatherTag
+
+        done < $TAGS_CATEGORIES_DIR/$motherCategory.txt
     done < $motherCategoryPath
 done
