1f871b8e4640158ab4bbcb9ff5159fe32ef37fc3
Tom Vautray <tom-vautray@hotmail.fr>
2012-01-01 21:37:16 +0100

    Add .webm files in fetch-names.sh and detect if extension is respected

--- fetch-names.sh
+++ fetch-names.sh
@@ -9,6 +9,7 @@ NAS_TARGETTED_DIR=$(cat nas-dir.txt)
 rm -rf $FILES_NAMES
 
 ssh $(cat ssh-host.txt) "find $NAS_TARGETTED_DIR/*/*.mp4" > $FILES_NAMES
+ssh $(cat ssh-host.txt) "find $NAS_TARGETTED_DIR/*/*.webm" >> $FILES_NAMES
 
 filesCount=$(wc -l < $FILES_NAMES)
 
@@ -21,3 +22,6 @@ grep -E " {2,}" $FILES_NAMES
 echo Files with square brackets:
 grep -E "\[" $FILES_NAMES
 grep -E "\]" $FILES_NAMES
+
+echo Files not ending with .mp4 or .webm:
+grep -Ev ".*\.(mp4|webm)" $FILES_NAMES | grep -v "@eaDir" | grep -v "desktop.ini"
