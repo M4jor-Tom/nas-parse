46aa3ba26661b65cce472838487bd763cdb05760
Tom Vautray <tom-vautray@hotmail.fr>
2024-04-08 20:01:44 +0200

    Rename scripts to know what they do better

--- nas-parse.sh
+++ nas-parse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 bash nas-import-names.sh
-bash split-valid-and-not-names.sh
+bash split-tagged-and-not-names.sh
 bash list-tags.sh
-bash validate-tags.sh
+bash validate-tags-with-categories.sh
