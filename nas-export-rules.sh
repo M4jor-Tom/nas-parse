a08dff30345a6f6034564c5da3bb99e9721be7ed
Tom Vautray <tom-vautray@hotmail.fr>
2024-05-23 23:35:02 +0200

    nas-export-rules.sh now sends a tar.gz archive

--- nas-export-rules.sh
+++ nas-export-rules.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+RULES_DIR=rules
+RULES_TAR=$RULES_DIR.tar.gz
+
+tar -czf $RULES_TAR $RULES_DIR
+
 NAS_TARGETTED_DIR=$(cat nas-dir.txt)
 
-scp -Or rules/ $(cat ssh-host.txt):"$NAS_TARGETTED_DIR"
+scp -Or $RULES_TAR $(cat ssh-host.txt):"$NAS_TARGETTED_DIR"
